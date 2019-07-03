fe62604fa23916168006f8fb6bd3741e6dea4d64
Максим <azeriker@gmail.com>
2019-07-03 18:43:14 +0300

    Version 0.1

---
+++ lib/home_screen.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:path/path.dart' show join;
+import 'package:path_provider/path_provider.dart';
+import 'report_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  final String login;
+  HomeScreen(this.login);
+
+ @override
+ State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<HomeScreen> {
+
+  int _currentIndex = 0;
+
+  final List<Widget> _children = [
+    ReportScreen(Colors.white),
+    ReportScreen(Colors.deepOrange),
+    ReportScreen(Colors.green)
+  ];
+
+  void onTabTapped(int index) {
+    setState(() {
+     _currentIndex = index;
+    });
+ }
+
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(
+       title: Text('Привет, ${widget.login}'),
+     ),
+     body: _children[_currentIndex],
+     bottomNavigationBar: BottomNavigationBar(
+       onTap: onTabTapped, 
+       currentIndex: _currentIndex,
+       items: [
+         BottomNavigationBarItem(
+           icon: new Icon(Icons.library_books),
+           title: new Text('Лента'),
+         ),
+         BottomNavigationBarItem(
+           icon: new Icon(Icons.send),
+           title: new Text('Сообщить'),
+         ),
+         BottomNavigationBarItem(
+           icon: Icon(Icons.account_box),
+           title: Text('Моя активность')
+         )
+       ],
+     ),
+   );
+ }
+}
---
+++ lib/login_screen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _loginScreenState();
+}
+
+class _loginScreenState extends State<LoginScreen> {
+  String _phone = "";
+  String _password = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: _buildBar(context),
+      body: new Container(
+        padding: EdgeInsets.all(16.0),
+        child: new Column(
+          children: <Widget>[
+            _buildTextFields(),
+            _buildButtons(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBar(BuildContext context) {
+    return new AppBar(
+      title: new Text("Вход"),
+      centerTitle: true,
+    );
+  }
+
+  Widget _buildButtons() {
+    return new Container(
+      child: new Column(
+        children: <Widget>[
+          new RaisedButton(
+            child: new Text('Войти'),
+            onPressed: _loginPressed,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTextFields() {
+    return new Container(
+      child: new Column(
+        children: <Widget>[
+          new Container(
+            child: new TextField(
+              onChanged: (t) => _phone = t,
+              maxLength: 11,
+              inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+              keyboardType: TextInputType.phone,
+              decoration: InputDecoration(
+                  labelText: 'Телефон', icon: Icon(Icons.phone_iphone),
+                  hintText: '79993332244'),
+            ),
+          ),
+          new Container(
+            child: new TextField(
+              onChanged: (t) => _password = t,
+              //controller: _passwordFilter,
+              decoration: new InputDecoration(
+                labelText: 'Пароль',
+                icon: Icon(Icons.lock),
+              ),
+              obscureText: true,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _loginPressed() {
+    print('The user wants to login with $_phone and $_password');
+    if(_phone=="79136479565"&& _password=="123")
+    {
+      Navigator.pushReplacementNamed(context, '/home/$_phone');
+    }
+  }
+}
